b5a93385927d0485688acef60130c97f9bd400a7
Lekshmi J <lekshmi0304@gmail.com>
2025-11-13 19:02:30 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Medical-Chatbot-with-LLMs-LangChain-Pinecone-Flask-AWS
+```bash
+conda create -n medibot python=3.10 -y
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Lekshmi J",
+    author_email="lekshmi0304@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
