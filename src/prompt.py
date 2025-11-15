2625fd1e0af92c26e15a5988e50e9176d4d7e490
Lekshmi J <lekshmi0304@gmail.com>
2025-11-15 10:50:33 +0530

    web app completed

---
+++ src/prompt.py
@@ -0,0 +1,10 @@
+system_prompt = (
+    "You are an Medical assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question.Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
+
